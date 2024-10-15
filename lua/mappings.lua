cf5de26e968214b6a7c9a03f87a1dfc548063591
Hossein Jafari <jafari@textkernel.nl>
2024-10-15 16:28:14 +0200

    My NvChad NVIM config

---
+++ lua/mappings.lua
@@ -0,0 +1,88 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map({ "n", "t" }, "<C-t>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
+end, { desc = "terminal new horizontal term" })
+
+vim.keymap.set("n", "<leader>k", '<cmd>lua require("kubectl").toggle()<cr>', { noremap = true, silent = true })
+
+-- vim.g.mapleader = " "
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- vim.keymap.set("n", "J", "mzJ`z")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
+--
+-- -- nnoremap <expr> <C-d> (winheight(0) / 3) . '<C-d>'
+-- -- nnoremap <expr> <C-u> (winheight(0) / 3) . '<C-u>'
+--
+-- vim.keymap.set("n", "n", "nzzzv")
+-- vim.keymap.set("n", "N", "Nzzzv")
+--
+-- -- vim.keymap.set("n", "<leader>vwm", function()
+-- --     require("vim-with-me").StartVimWithMe()
+-- -- end)
+-- -- vim.keymap.set("n", "<leader>svwm", function()
+-- --     require("vim-with-me").StopVimWithMe()
+-- -- end)
+--
+-- -- greatest remap ever
+-- vim.keymap.set("x", "<leader>p", [["_dP]])
+--
+-- -- next greatest remap ever : asbjornHaland
+-- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>Y", [["+Y]])
+--
+-- vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+-- -- 
+-- -- -- This is going to get me cancelled
+-- vim.keymap.set("i", "<C-c>", "<Esc>")
+-- -- 
+-- -- vim.keymap.set("n", "Q", "<nop>")
+-- -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- -- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+-- -- 
+-- -- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- -- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- -- 
+-- -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- -- 
+-- -- vim.keymap.set(
+-- --     "n",
+-- --     "<leader>ee",
+-- --     "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+-- -- )
+-- -- 
+-- -- vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+-- -- vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
+-- -- 
+-- -- vim.keymap.set("n", "<leader><leader>", function()
+-- --     vim.cmd("so")
+-- -- end)
+--
+--
+-- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+-- vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+-- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+-- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+-- vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+-- vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+-- vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+-- vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+-- vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+-- vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--
